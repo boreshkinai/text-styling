df1ae6116d86cddd7c989e3c30bac913aec34be1
Boris Oreshkin <boris@elementai.com>
2017-05-04 09:10:03 -0400

    Adding complete sentences to the list of complete

--- beam_search.py
+++ beam_search.py
@@ -45,10 +45,12 @@ sentences_wmt = np.array(utils.split_into_sentences(text_wmt))
 sentences_wmt = sorted(sentences_wmt, key=len)
 chars_wmt = sorted(list(set("".join(sentences_wmt))))
 
-print('total chars, Shakespear:', len(chars_shakespeare))
+print('total chars, Shakespeare:', len(chars_shakespeare))
 print('total chars, WMT:', len(chars_wmt))
 
 chars = sorted(list(set(chars_wmt + chars_shakespeare)))
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
 
 def beam_search(model, sentence_enc, start_frag_enc, beam_width, num_candidates):
     num_chars = sentence_enc.shape[-1]
@@ -59,25 +61,33 @@ def beam_search(model, sentence_enc, start_frag_enc, beam_width, num_candidates)
     complete_sentences = []
     complete_scores = []
 
-
     # candidate_symbols = (-prediction).argsort(axis=None)[:beam_width]
     while len(complete_sentences) < num_candidates:
 
         score_matrix = -np.inf * np.ones((num_chars, beam_width))
         for i, candidate in candidates.items():
-            p = model.predict([sentence_enc, candidate])[0,-1,:]
-            score_matrix[:,i] = np.log(p + EPS) + candidate_scores[i]
+            p = model.predict([sentence_enc, candidate])[0, -1, :]
+            p[p < 0.000001] = 0.000001
+            p[p > 0.999999] = 0.999999
+            score_matrix[:, i] = np.log(p + EPS) + candidate_scores[i]
         new_symbols = (-score_matrix).argsort(axis=None)[:beam_width]
         idxs = np.unravel_index(new_symbols, score_matrix.shape)
 
-        count=0
+        count = 0
         new_candidates = {}
         new_candidate_scores = {}
         for idx in zip(idxs[0], idxs[1]):
             new_symbol_enc = np.zeros((1, 1, num_chars))
             new_symbol_enc[0, 0, idx[0]] = 1
+
             new_candidates[count] = np.concatenate([candidates[idx[1]], new_symbol_enc], axis=1)
             new_candidate_scores[count] = score_matrix[idx[0], idx[1]]
+
+            if (indices_char[idx[0]] == utils.SENTENCE_END):
+                complete_sentences.append(new_candidates[count])
+                complete_scores.append(new_candidate_scores[count])
+                new_candidate_scores[count] = -1e10
+
             count += 1
 
         candidates = new_candidates
@@ -85,27 +95,34 @@ def beam_search(model, sentence_enc, start_frag_enc, beam_width, num_candidates)
 
         for i, candidate in candidates.items():
             sentence = utils.sentence_decode(candidate, chars)
-            print("Candidate %s: " %(i), sentence, 'Score: ', candidate_scores[i])
+            print("Candidate %s: " % (i), sentence, 'Score: ', candidate_scores[i])
+        print()
+
+        for i, candidate in enumerate(complete_sentences):
+            sentence = utils.sentence_decode(candidate, chars)
+            print("Complete %s: " % (i), sentence, 'Score: ', complete_scores[i])
         print()
 
 
+MODEL_NAME = 'model_shakespeare.hd5'
+#MODEL_NAME = 'model_wmt.hd5'
 
 test_sentence = '#have i forgotten myself so far that i have not even told you his name ?_'
 test_sentence = "#i do not know what you are talking about !_"
+test_sentence = "#i believe robots are the future !_"
+test_sentence = "#this is not important ._"
+#test_sentence = "#please let me live in piece !_"
 
 #test_sentence = "#i do not understand you !_"
-test_sentence = "#what can i tell you , my friend ?_"
-#test_sentence = "#a house is a building that functions as a home ._"
 
-
-MODEL_NAME = 'model_shakespeare.hd5'
-MODEL_NAME = 'model_wmt.hd5'
 
 model = load_model(MODEL_NAME)
+# model = wmt_autoencoder
+# model = shakespeare_autoencoder_forward
 
 sentence_enc = utils.sentence_encode(test_sentence, chars)
 fragment_enc = utils.sentence_encode('#', chars)
 
-sentence_predicted = beam_search(model, sentence_enc, fragment_enc, beam_width=10, num_candidates=5)
+sentence_predicted = beam_search(model, sentence_enc, fragment_enc, beam_width=20, num_candidates=20)
 
-a=1;
+a = 1;
